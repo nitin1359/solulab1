5ec7ceb28dd2eb5084b116219992b68d15c18b42
nitin1359 <nitinshukla135@gmail.com>
2024-06-24 14:39:37 +0530

    All good till here, just foreground notification images are not coming in collapsed view, and coming at both(full image, and small icon at right top) places.

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,17 @@ import 'package:solulab1/screens/authentication/signup_main.dart';
 import 'package:solulab1/navigation/wrapper.dart';
 import 'package:solulab1/services/notification_services.dart';
 
-
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler); 
-  
-  runApp(const MyApp()); 
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  runApp(const MyApp());
 }
+
 @pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
@@ -34,7 +33,6 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   }
 }
 
-
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -43,24 +41,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-NotificationServices notificationServices = NotificationServices();
+  NotificationServices notificationServices = NotificationServices();
   @override
   void initState() {
-    
     super.initState();
-    notificationServices .requestNotificationPermission();
-    notificationServices.foregroundMessage();
-    // notificationServices .isTokenRefresh();
-    notificationServices .firebaseInit(context);
-    notificationServices .getDeviceToken().then((value){
+
+    notificationServices.requestNotificationPermisions();
+    notificationServices.forgroundMessage();
+    notificationServices.firebaseInit(context);
+    notificationServices.setupInteractMessage(context);
+    notificationServices.isRefreshToken();
+    notificationServices.getDeviceToken().then((value) {
       if (kDebugMode) {
-        print('FCM Token: $value');
+        print(value);
       }
     });
-    
-
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -1,147 +1,176 @@
-// ignore_for_file: avoid_print
+// ignore_for_file: use_build_context_synchronously, avoid_print
 import 'dart:convert';
 import 'dart:io';
-import 'dart:math';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:http/http.dart' as http;
-import 'package:solulab1/services/message_screen.dart';
 
 class NotificationServices {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
+
   final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
-  void requestNotificationPermission() async {
-    NotificationSettings settings = await messaging.requestPermission(
-        alert: true,
-        announcement: true,
-        badge: true,
-        carPlay: true,
-        criticalAlert: true,
-        provisional: true,
-        sound: true);
-    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-      print('user granted permission');
-    } else if (settings.authorizationStatus ==
-        AuthorizationStatus.provisional) {
-      print('user granted provisional permission');
-    } else {
-      print('user denied permission');
+  Future<String> getDeviceToken() async {
+    String? token = await messaging.getToken();
+    return token!;
+  }
+
+  void isRefreshToken() async {
+    messaging.onTokenRefresh.listen((event) {
+      event.toString();
+      print('Token Refereshed');
+    });
+  }
+
+  void requestNotificationPermisions() async {
+    if (Platform.isIOS) {
+      await messaging.requestPermission(
+          alert: true,
+          announcement: true,
+          badge: true,
+          carPlay: true,
+          criticalAlert: true,
+          provisional: true,
+          sound: true);
     }
+
+    NotificationSettings notificationSettings =
+        await messaging.requestPermission(
+            alert: true,
+            announcement: true,
+            badge: true,
+            carPlay: true,
+            criticalAlert: true,
+            provisional: true,
+            sound: true);
+
+    if (notificationSettings.authorizationStatus ==
+        AuthorizationStatus.authorized) {
+      print('user is already granted permisions');
+    } else if (notificationSettings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('user is already granted provisional permisions');
+    } else {
+      print('User has denied permission');
+    }
+  }
+
+  // For IoS
+  Future forgroundMessage() async {
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+            alert: true, badge: true, sound: true);
+  }
+
+  void firebaseInit(BuildContext context) {
+    FirebaseMessaging.onMessage.listen((message) {
+      print("Notification title: ${message.notification!.title}");
+      print("Notification title: ${message.notification!.body}");
+      print("Data: ${message.data}");
+
+      if (Platform.isIOS) {
+        forgroundMessage();
+      }
+
+      if (Platform.isAndroid) {
+        initLocalNotifications(context, message);
+        showNotification(message);
+      }
+    });
   }
 
   void initLocalNotifications(
       BuildContext context, RemoteMessage message) async {
-    var androidInitializationSettings =
-        AndroidInitializationSettings('@mipmap/ic_launcher');
-    var iosInitializationSettings = DarwinInitializationSettings();
+    var androidInitSettings =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iosInitSettings = const DarwinInitializationSettings();
 
-    var initializationSettings = InitializationSettings(
-      android: androidInitializationSettings,
-      iOS: iosInitializationSettings,
+    var initSettings = InitializationSettings(
+      android: androidInitSettings,
+      iOS: iosInitSettings,
     );
-    await _flutterLocalNotificationsPlugin.initialize(
-      initializationSettings,
-      onDidReceiveNotificationResponse: (payload) async {
-        handleMessage(context, message);
-      },
-    );
+
+    await _flutterLocalNotificationsPlugin.initialize(initSettings,
+        onDidReceiveNotificationResponse: (payload) {
+      handleMesssage(context, message);
+    });
   }
 
-  void firebaseInit(BuildContext context) {
-    FirebaseMessaging.onMessage.listen(
-      (RemoteMessage message) {
-        print(message.notification!.title.toString());
-        print(message.notification!.body.toString());
-        if (Platform.isIOS) {
-          foregroundMessage();
-        }
-        if (Platform.isAndroid) {
-          initLocalNotifications(context, message);
-          showNotification(message);
-        }
-      },
-    );
-  }
-
-  Future<void> showNotification(RemoteMessage message) async {
-    AndroidNotificationChannel channel = AndroidNotificationChannel(
-      Random.secure().nextInt(100000).toString(), // id
-      'High Importance Notifications', // title
-      importance: Importance.max,
-    );
-
-    final http.Response response = await http.get(Uri.parse("https://goodfavorites.com/wp-content/uploads/2020/02/candy-birthday-cakes-lovely-just-jenny-lynne-happy-national-cake-day-of-candy-birthday-cakes.jpg"));
-    BigPictureStyleInformation bigPictureStyleInformation =
-        BigPictureStyleInformation(
-      ByteArrayAndroidBitmap.fromBase64String(base64Encode(response.bodyBytes)),
-      largeIcon: ByteArrayAndroidBitmap.fromBase64String(base64Encode(response.bodyBytes)),
-    );
-    AndroidNotificationDetails androidNotificationDetails =
-        AndroidNotificationDetails(
-      channel.id,
-      channel.name, 
-      styleInformation:  bigPictureStyleInformation,
-      importance: Importance.high,
-      priority: Priority.high,
-      ticker: 'ticker',
-    );
-
-    DarwinNotificationDetails darwinNotificationDetails =
-        DarwinNotificationDetails(
-      presentAlert: true,
-      presentBadge: true,
-      presentSound: true,
-    );
-
-    NotificationDetails notificationDetails = NotificationDetails(
-      android: androidNotificationDetails,
-      iOS: darwinNotificationDetails,
-    );
-
-    Future.delayed(
-      Duration.zero,
-      () {
-        _flutterLocalNotificationsPlugin.show(
-          0,
-          message.notification!.title.toString(),
-          message.notification!.body.toString(),
-          notificationDetails,
-        );
-      },
-    );
-  }
-
-  Future<String> getDeviceToken() async {
-    String? token = await messaging.getToken();
-    print("token: $token");
-    return token!;
-  }
-
-  void isTokenRefresh() async {
-    messaging.onTokenRefresh.listen(
-      (event) {
-        event.toString();
-        print("token: $event");
-      },
-    );
-  }
-
-  void handleMessage(BuildContext context, RemoteMessage message) {
-    if (message.data['1'] == '1') {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => MessageScreen()));
+  void handleMesssage(BuildContext context, RemoteMessage message) {
+    if (kDebugMode) {
+      print('In handle Messsage function');
+    }
+    if (message.data['type'] == 'text') {
+      // redirect to new screen or take different action based on payload that we receive.
     }
   }
 
-  Future foregroundMessage() async {
-    await FirebaseMessaging.instance
-        .setForegroundNotificationPresentationOptions(
-      alert: true,
-      badge: true,
-      sound: true,
+  Future<void> showNotification(RemoteMessage message) async {
+    String? imageUrl = message.notification?.android?.imageUrl; // Here do not use final with this string to show new image from firebase every time.
+    final http.Response? response =
+        imageUrl != null ? await http.get(Uri.parse(imageUrl)) : null;
+    final byteArray = response?.bodyBytes;
+
+    BigPictureStyleInformation bigPictureStyleInformation =
+        BigPictureStyleInformation(
+      ByteArrayAndroidBitmap.fromBase64String(base64Encode(byteArray!)),
+      largeIcon:
+          ByteArrayAndroidBitmap.fromBase64String(base64Encode(byteArray)),
     );
+
+    AndroidNotificationChannel androidNotificationChannel =
+        AndroidNotificationChannel(
+      message.notification!.android!.channelId.toString(),
+      message.notification!.android!.channelId.toString(),
+      importance: Importance.max,
+      showBadge: true,
+      playSound: true,
+    );
+
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(
+      androidNotificationChannel.id.toString(),
+      androidNotificationChannel.name.toString(),
+      channelDescription: 'Flutter Notifications',
+      importance: Importance.max,
+      priority: Priority.high,
+      playSound: true,
+      ticker: 'ticker',
+      sound: androidNotificationChannel.sound,
+      styleInformation: bigPictureStyleInformation,
+    );
+
+    const DarwinNotificationDetails darwinNotificationDetails =
+        DarwinNotificationDetails(
+            presentAlert: true, presentBadge: true, presentSound: true);
+
+    NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails, iOS: darwinNotificationDetails);
+
+    Future.delayed(Duration.zero, () {
+      _flutterLocalNotificationsPlugin.show(
+          0,
+          message.notification!.title.toString(),
+          message.notification!.body.toString(),
+          notificationDetails);
+    });
+  }
+
+  Future<void> setupInteractMessage(BuildContext context) async {
+    // when app is terminated
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+
+    if (initialMessage != null) {
+      handleMesssage(context, initialMessage);
+    }
+
+    //when app is in background
+    FirebaseMessaging.onMessageOpenedApp.listen((event) {
+      handleMesssage(context, event);
+    });
   }
 }
